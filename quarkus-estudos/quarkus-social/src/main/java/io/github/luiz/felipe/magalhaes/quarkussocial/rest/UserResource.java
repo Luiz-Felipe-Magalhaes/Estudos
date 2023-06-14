66d8f013859eacab87cf3d9700d879a5c334ab3f
Luiz Felipe Magalhães <luizfelipe.magalhaes@outlook.com>
2023-06-14 13:48:23 -0300

    Organização de pastas e adição de novos estudos

---
+++ quarkus-estudos/quarkus-social/src/main/java/io/github/luiz/felipe/magalhaes/quarkussocial/rest/UserResource.java
@@ -0,0 +1,36 @@
+package io.github.luiz.felipe.magalhaes.quarkussocial.rest;
+
+import io.github.luiz.felipe.magalhaes.quarkussocial.domain.model.User;
+import io.github.luiz.felipe.magalhaes.quarkussocial.rest.dto.CreateUserRequest;
+import io.quarkus.hibernate.orm.panache.PanacheQuery;
+
+import javax.transaction.Transactional;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/users")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class UserResource {
+
+    @POST
+    @Transactional
+    public Response createUser(CreateUserRequest userRequest){
+        User user = new User();
+        user.setName(userRequest.getName());
+        user.setAge(userRequest.getAge());
+
+        user.persist();
+
+        return Response.ok(user).build();
+    }
+
+    @GET
+    public Response listAllUsers() {
+        PanacheQuery<User> query = User.findAll();
+
+        return Response.ok(query.list()).build();
+    }
+
+}
